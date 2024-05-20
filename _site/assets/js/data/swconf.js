10ff182b339274103aaa69a4e1805bdd758d72ac
4ndymcfly <4ndymcfly@gmail.com>
2024-05-20 14:33:42 +0200

    Corrección de enlaces internos en el sitio web

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1716206991',resources: [
+    cacheName: 'chirpy-1716208229',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
